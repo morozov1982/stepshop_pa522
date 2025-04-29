7d6a3083087b3cd38d1800368ec289a015e88bb9
morozov1982 <morozov1982@gmail.com>
2025-04-29 18:24:08 +0500

    lesson_04: context

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    title = 'Главная'
+
+    context = {
+        'title': title,
+    }
+
+    return render(request, 'index.html', context)
 
 def contacts(request):
     return render(request, 'contacts.html')
