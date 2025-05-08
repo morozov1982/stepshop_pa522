4970b23d93e340ec4ec9d73d55f73b466cf22859
morozov1982 <morozov1982@gmail.com>
2025-05-08 18:25:13 +0500

    lesson_06: работа с данными

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,14 +1,44 @@
 from django.shortcuts import render
 
+from mainapp.models import Product
+
+
 def index(request):
     title = 'Главная'
 
+    prods = Product.objects.all()[:4]
+
     context = {
         'title': title,
+        'products': prods,
     }
 
     return render(request, 'index.html', context)
 
 def contacts(request):
-    return render(request, 'contacts.html')
+    title = 'Контакты'
+    context = {
+        'title': title,
+    }
+    return render(request, 'contacts.html', context)
 
+def about(request):
+    title = 'О нас'
+    context = {
+        'title': title,
+    }
+    return render(request, 'about.html', context)
+
+def products(request):
+    title = 'Товары'
+    context = {
+        'title': title,
+    }
+    return render(request, 'products.html', context)
+
+def product(request):
+    title = 'Товар'
+    context = {
+        'title': title,
+    }
+    return render(request, 'product.html', context)
