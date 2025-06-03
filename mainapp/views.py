9757e1175d4ddae685980cf97c859349415a8ebc
Морозов Александр <@step.kz>
2025-06-03 18:41:32 +0500

    lesson_11: product, main_menu

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render
 from mainapp.models import Product, Category
 
 
+def get_main_menu(current='mainapp:index'):
+    return [
+        {'href': 'mainapp:index', 'name': 'Главная', 'active': current},
+        {'href': 'mainapp:products', 'name': 'Товары', 'active': current},
+        {'href': 'mainapp:about', 'name': 'О нас', 'active': current},
+        {'href': 'mainapp:contacts', 'name': 'Контакты', 'active': current},
+    ]
+
+
 def index(request):
     title = 'Главная'
 
@@ -11,6 +20,7 @@ def index(request):
     context = {
         'title': title,
         'products': prods,
+        'menu_links': get_main_menu(),
     }
 
     return render(request, 'index.html', context)
@@ -19,6 +29,7 @@ def contacts(request):
     title = 'Контакты'
     context = {
         'title': title,
+        'menu_links': get_main_menu('mainapp:contacts'),
     }
     return render(request, 'contacts.html', context)
 
@@ -26,6 +37,7 @@ def about(request):
     title = 'О нас'
     context = {
         'title': title,
+        'menu_links': get_main_menu('mainapp:about'),
     }
     return render(request, 'about.html', context)
 
@@ -38,18 +50,20 @@ def products(request):
         'title': title,
         'products': prods,
         'categories': categories,
+        'menu_links': get_main_menu('mainapp:products'),
     }
     return render(request, 'products.html', context)
 
-def product(request):
+def product(request, pk):
     title = 'Товар'
 
-    prod = Product.objects.get(id=1)
-    same_prods = Product.objects.exclude(id=prod.id)
+    prod = Product.objects.get(id=pk)
+    same_prods = Product.objects.exclude(id=pk)
 
     context = {
         'title': title,
         'product': prod,
         'products': same_prods,
+        'menu_links': get_main_menu('mainapp:products'),
     }
     return render(request, 'product.html', context)
